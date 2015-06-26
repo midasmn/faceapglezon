e11390f99518953922d03410b9e3b83a08d70663
midasmn <midasmn@gmail.com>
2015-06-27 00:15:11 +0900

    crontab

--- movie-png/cron_del.php
+++ movie-png/cron_del.php
@@ -10,38 +10,38 @@ if(!$db_conn = mysql_connect($host, $user, $pass))
 // $rtn = mysql_query("SET NAMES sjis" , $db_conn);
 mysql_select_db($dbname);
 
-// function f_update_flg($db_conn,$exmname)
-// {
-// 	$sql = "UPDATE `mpegmagick` SET `FLG` = 'DEL' WHERE `exmname` = '$exmname'";
-// 	$result = mysql_query($sql, $db_conn);
-// // var_dump($result);
-// 	if(!$result)
-// 	{
-// 		// die('Invalid query: ' . mysql_error());
-// 		$rtn =  "NG";
-// 	}else{
-// 		$rtn = "OK";
-// 	}
-// 	return $rtn;
-// }
-// //
-// function remove_directory($dir) {
-//   if ($handle = opendir("$dir")) {
-//    while (false !== ($item = readdir($handle))) {
-//      if ($item != "." && $item != "..") {
-//        if (is_dir("$dir/$item")) {
-//          remove_directory("$dir/$item");
-//        } else {
-//          unlink("$dir/$item");
-//          // echo " removing $dir/$item<br>\n";
-//        }
-//      }
-//    }
-//    closedir($handle);
-//    rmdir($dir);
-//    // echo "removing $dir<br>\n";
-//   }
-// }
+function f_update_flg($db_conn,$exmname)
+{
+	$sql = "UPDATE `mpegmagick` SET `FLG` = 'DEL' WHERE `exmname` = '$exmname'";
+	$result = mysql_query($sql, $db_conn);
+// var_dump($result);
+	if(!$result)
+	{
+		// die('Invalid query: ' . mysql_error());
+		$rtn =  "NG";
+	}else{
+		$rtn = "OK";
+	}
+	return $rtn;
+}
+//
+function remove_directory($dir) {
+  if ($handle = opendir("$dir")) {
+   while (false !== ($item = readdir($handle))) {
+     if ($item != "." && $item != "..") {
+       if (is_dir("$dir/$item")) {
+         remove_directory("$dir/$item");
+       } else {
+         unlink("$dir/$item");
+         // echo " removing $dir/$item<br>\n";
+       }
+     }
+   }
+   closedir($handle);
+   rmdir($dir);
+   // echo "removing $dir<br>\n";
+  }
+}
 
 
 //処理フォルダ
@@ -57,7 +57,12 @@ while($link = mysql_fetch_row($result))
 	list($exmname) = $link;
 	//ディレクトリ削除
 	// $delpath = $TMP.$exmname;
-	remove_directory($exmname);
+	remove_directory("/usr/share/nginx/html/faceapglezon.info/movie-gif/tmp/".$exmname);
+	remove_directory("/usr/share/nginx/html/faceapglezon.info/movie-gif/uploads/".$exmname);
+	remove_directory("/usr/share/nginx/html/faceapglezon.info/movie-png/tmp/".$exmname);
+	remove_directory("/usr/share/nginx/html/faceapglezon.info/movie-png/uploads/".$exmname);
+	remove_directory("/usr/share/nginx/html/faceapglezon.info/soundonly/tmp/".$exmname);
+	remove_directory("/usr/share/nginx/html/faceapglezon.info/soundonly/uploads/".$exmname);
 	//フラグアップデート
 	f_update_flg($db_conn,$exmname);
 }
